eaa135b4b26c59c1e70ee7d21e35bbcb4ff72111
Mohamed Ali <115352232+ES-Mohamed609@users.noreply.github.com>
2024-08-05 10:47:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:yourpassword@localhost/ContactApp'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456789.@localhost/contactpp'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
